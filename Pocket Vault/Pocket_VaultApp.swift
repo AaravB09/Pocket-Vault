6ee0bc7c7dda1908a7c6799cc6af65448231aadd
Aarav B <aarav09bansal@gmail.com>
2026-08-02 20:31:54 -0700

    Initial Commit

---
+++ Pocket Vault/Pocket_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_VaultApp.swift
+//  Pocket Vault
+//
+//  Created by Aarav Bansal on 2026-08-02.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
